99fedd5cfa4ad271cb593441e19650c7125b8671
Lane Brooks <lane@macair.lane.brooks.nu>
2016-05-31 14:13:59 -0600

    Added initial i2c host interface. Not functional yet

---
+++ models/i2c_host.v
@@ -0,0 +1,333 @@
+/**
+ * Copyright (C) 2013 BrooksEE, LLC.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ **/
+
+`define FX3_READ_BUFFERS 14
+`define FX3_WRITE_BUFFERS 1
+`define FX3_BUF_MULTIPLIER 2
+
+module i2c_host
+  (
+   input 	 clk,
+   
+   inout 	 SCL,
+   inout 	 SDA
+   );
+
+   reg sloe_b;
+   reg hics_b             /* verilator public */;
+   reg [31:0] rbuf[0:256*`FX3_READ_BUFFERS*`FX3_BUF_MULTIPLIER-1] /* verilator public */;
+   reg [15:0]  rptr        /* verilator public */;
+   reg rdone              /* verilator public */;
+   reg [31:0] wbuf[0:256*`FX3_WRITE_BUFFERS*`FX3_BUF_MULTIPLIER-1] /* verilator public */;
+   reg [31:0] cbuf[0:3]   /* verilator public */;
+   reg [15:0]  wptr        /* verilator public */;
+   reg [15:0]  wend        /* verilator public */;
+   reg empty_b            /* verilator public */;
+   reg cmd_empty_b        /* verilator public */;
+   reg [2:0] cmd_ptr  /* verilator public */ = 4;
+   reg full_b             /* verilator public */;
+   reg [31:0] datao;
+   reg [31:0] cmd_datao;
+
+   pullup p1(SCL);
+   pullup p2(SDA);
+
+   reg [1:0] fifo_addr;
+   assign fx3_ifclk  = clk; // invert clk
+   assign fx3_clkout = clk;
+   assign fx3_hics_b = hics_b;
+   
+   wire        wfifo_active    = fifo_addr == 3;
+   wire        rfifo_active    = fifo_addr == 0;
+   wire        cmd_fifo_active = fifo_addr == 2;
+
+   wire        slwr_b    = !(!fx3_slwr_b && rfifo_active);
+
+   reg [31:0]  datao1;
+   
+
+//   assign fx3_flags = { 1'b0, full_b, empty_b };
+   
+   assign fx3_dma_rdy_b = wfifo_active    ? !empty_b :
+			  cmd_fifo_active ? !cmd_empty_b :
+			  rfifo_active    ? !full_b :
+			  1'b1;
+
+   assign fx3_fd = (sloe_b) ? 32'hZZZZZZZZ : datao1;
+   wire [31:0] fd_in = fx3_fd;
+   
+//   assign fx3_fd_out = datao1;
+//   assign fx3_fd_oe  = !sloe_b;
+//   wire [31:0] fd_in = fx3_fd_in;
+   
+   always @(posedge clk) begin
+      fifo_addr <= fx3_fifo_addr;
+      sloe_b    <= fx3_sloe_b;
+      
+      empty_b <= !(wptr >= wend);
+      if(!fx3_slrd_b && wfifo_active) begin
+         wptr  <= wptr + 1;
+         /* verilator lint_off WIDTH */
+         datao <= wbuf[wptr];
+         /* verilator lint_on WIDTH */
+      end
+
+      cmd_empty_b <= !(cmd_ptr >= 4);
+      if(!fx3_slrd_b && cmd_empty_b && cmd_fifo_active) begin
+	 cmd_ptr <= cmd_ptr+1;
+         cmd_datao <= cbuf[cmd_ptr[1:0]];
+      end
+
+      datao1 <= wfifo_active    ? datao : 
+	        cmd_fifo_active ? cmd_datao : 0;
+      
+      full_b <= !((rptr > `FX3_READ_BUFFERS*`FX3_BUF_MULTIPLIER*256-1 ) || rdone);
+      if(!slwr_b && (rptr <= `FX3_READ_BUFFERS*`FX3_BUF_MULTIPLIER*256-1 )) begin
+         /* verilator lint_off WIDTH */
+         rbuf[rptr] <= fd_in;
+         /* verilator lint_on WIDTH */
+         rptr <= rptr + 1;
+      end
+
+      if(!fx3_pktend_b) begin
+         rdone <= 1;
+      end
+   end
+
+   initial begin
+      hics_b=1;
+      rptr=0;
+      rdone=0;
+      wptr=0;
+      wend=0;
+      datao=0;
+   end
+
+`ifndef verilator
+/***********************************************************/
+/********************** GENERIC VERILOG MODEL **************/
+/***********************************************************/
+   
+    integer i;
+    integer txcount;
+    integer rxcount;
+
+    wire [15:0] rd_buf_size = 1024*`FX3_READ_BUFFERS*`FX3_BUF_MULTIPLIER; 
+    wire [15:0] wr_buf_size = 1024*`FX3_WRITE_BUFFERS*`FX3_BUF_MULTIPLIER; 
+    // final destination buffer for reads and writes
+    reg [7:0] rdwr_data_buf[0:1024*`FX3_READ_BUFFERS*`FX3_BUF_MULTIPLIER-1];
+
+   /**
+    * each io call sends a command to the FPGA
+    **/
+   task _sendcmd;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      input [7:0]  cmd;
+      input [31:0] length;
+      begin
+         hics_b=1;
+         repeat (50) @(posedge clk);
+         hics_b=0;
+         repeat (20) @(posedge clk);
+         cbuf[0] = {
+             cmd == 1 ? rd_buf_size :
+                        wr_buf_size,
+                        8'hc3, cmd };
+         cbuf[1] = { 16'b0, term_addr };
+         cbuf[2] = reg_addr;
+         cbuf[3] = length;
+         cmd_ptr = 0;
+         wptr = 0;
+         wend = 0;
+         rptr = 0;
+         rdone = 0;
+         repeat (3) @(posedge clk);
+      end
+   endtask
+
+
+   /**
+    * Simulate an FX3 get command.
+    **/
+   task get;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      output [31:0] value;
+      begin
+         read(term_addr,reg_addr, 4);
+         value = { rdwr_data_buf[3], rdwr_data_buf[2], rdwr_data_buf[1], rdwr_data_buf[0] };
+      end
+   endtask
+
+   // Get a register wider than 32b. specify the width of the register in
+   // in bits and this will loop through starting at 'reg_addr' doing 16b
+   // gets until it has retrieved all words in the wide register. Return
+   // value has a max of 1024 bits.
+   task getW;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      input [9:0] width;
+      output [1023:0] value;
+      integer        wcount;
+      begin
+         value = 0;//clear out the return value first
+         for(wcount=0; wcount<width; wcount=wcount+32) begin // loop through reg
+            read(term_addr,reg_addr+(wcount/32), 4);
+            value = value | ({ rdwr_data_buf[3], rdwr_data_buf[2], rdwr_data_buf[1], rdwr_data_buf[0] } << wcount);
+            `ifdef DEBUG_FX3 
+              $display("%d getW: wcount=%d buf[0]=0x%x buf[1]=0x%x buf[2]=0x%x buf[3]=0x%x",$time, wcount, rdwr_data_buf[0], rdwr_data_buf[1], rdwr_data_buf[2], rdwr_data_buf[3]);
+            `endif
+         end
+        `ifdef DEBUG_FX3 
+         $display("%d getW: value=0x%x",$time, value);
+        `endif
+      end
+   endtask
+
+
+   
+   /**
+    * Simulate an FX3 set command
+    **/
+   task set;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      input [31:0] value;
+      begin
+         rdwr_data_buf[0] = value[7:0];
+         rdwr_data_buf[1] = value[15:8];
+         rdwr_data_buf[2] = value[23:16];
+         rdwr_data_buf[3] = value[31:24];
+         write(term_addr,reg_addr,4);
+      end
+   endtask
+
+   // Set a wide register. See getW() documentation.
+   task setW;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      input [9:0] width;
+      input [1023:0] value;
+      integer        wcount;
+      for(wcount=0; wcount<width; wcount=wcount+32) begin
+         rdwr_data_buf[0] = 8'hFF & (value >> wcount);
+         rdwr_data_buf[1] = 8'hFF & (value >> (wcount+8));
+         rdwr_data_buf[2] = 8'hFF & (value >> (wcount+16));
+         rdwr_data_buf[3] = 8'hFF & (value >> (wcount+23));
+         write(term_addr,reg_addr+(wcount/32),4);
+      end
+   endtask
+
+
+   /**
+    * Simulate an FX3 read command
+    **/
+   task read;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      input [31:0] length;
+      begin
+         _sendcmd( term_addr, reg_addr, 1, length ); 
+         rxcount = 0;
+         while(rxcount < length + 8) begin 
+            if(rdone || (full_b==0)) begin 
+               repeat (10) @(posedge clk);
+
+               for(i=0; i<rptr; i=i+1) begin 
+                  rdwr_data_buf[rxcount] = rbuf[i][7:0];
+                  rdwr_data_buf[rxcount+1] = rbuf[i][15:8];
+                  rdwr_data_buf[rxcount+2] = rbuf[i][23:16];
+                  rdwr_data_buf[rxcount+3] = rbuf[i][31:24];
+                  rxcount = rxcount + 4;
+               end 
+               rdone = 0;
+               rptr  = 0;
+            end 
+            @(posedge clk);
+            
+            //if(main_time >= timeout_time) {
+            //  free(rx_data);                         
+            //  throw Exception(USB_COMM, "Timed out");
+            //}
+         end 
+      end
+   endtask
+
+   /**
+    * Simulate an FX3 write command
+    **/
+   task write;
+      input [15:0] term_addr;
+      input [31:0] reg_addr;
+      input [31:0] length;
+      begin
+         _sendcmd ( term_addr, reg_addr, 2, length );
+         txcount = 0;
+         
+         while (txcount < length) begin
+            while ( empty_b ) begin
+               @(posedge clk);
+            end
+            for (i=0;i<256 && txcount < length; i=i+1) begin
+               wbuf[i] = { rdwr_data_buf[txcount+3], rdwr_data_buf[txcount+2], rdwr_data_buf[txcount+1] , rdwr_data_buf[txcount] };
+               txcount = txcount + 4;
+            end
+            wptr = 0;
+            wend = i;
+
+            repeat(4) @(posedge clk);
+         end
+
+         // wait for ack
+         while (rptr < 2) begin
+            @(posedge clk);
+         end
+         
+         repeat (10) @(posedge clk);
+      end
+   endtask
+
+`else 
+/***********************************************************/
+/********************** VERILATOR CODE *********************/
+/***********************************************************/
+
+ `systemc_header
+#include "../../../../lib/HostInterface/models/fx3_verilator.cpp"
+ `systemc_interface
+   FX3Device *fx3_dev;    // Pointer to object we are embedding
+ `systemc_ctor
+   fx3_dev = new FX3Device(); // Construct contained object
+   fx3_dev->cbuf = cbuf;
+   fx3_dev->wbuf = wbuf;
+   fx3_dev->rbuf = rbuf;
+   fx3_dev->wptr = &wptr;
+   fx3_dev->rptr = &rptr;
+   fx3_dev->cmd_ptr = &cmd_ptr;
+   fx3_dev->wend = &wend;
+   fx3_dev->rdone= &rdone;
+   fx3_dev->hics_b= &hics_b;
+   fx3_dev->full_b= &full_b;
+   fx3_dev->empty_b= &empty_b;
+ `systemc_dtor
+   delete fx3_dev;    // Destruct contained object
+ `verilog
+`endif
+   
+endmodule
---
+++ rtl/i2cHostInterface.v
@@ -0,0 +1,290 @@
+// Author: Lane Brooks
+// Date: May 26, 2016
+//
+// Description: This implemenents a i2c slave to host interface
+// controller.  This will match any i2c chip address and set it as the
+// terminal address.  A future implementation could take in an array
+// of terminal addresses to match.
+
+
+module i2c_slave
+  #(parameter NUM_ADDR_BYTES=1,
+    parameter NUM_DATA_BYTES=2,
+    parameter REG_ADDR_WIDTH=8*NUM_ADDR_BYTES,
+    parameter REG_DATA_WIDTH=8*NUM_DATA_BYTES)
+   (
+    input 			    clk,
+    input 			    reset_n,
+    input 			    sda_in,
+    output 			    sda_out,
+    output 			    sda_oeb,
+    input 			    scl_in,
+    output 			    scl_out,
+    output 			    scl_oeb
+
+    output reg [15:0] 		    di_term_addr,
+    output reg [31:0] 		    di_reg_addr,
+    output [31:0] 		    di_len,
+
+    output reg 			    di_read_mode,
+    output reg 			    di_read_req,
+    output reg 			    di_read,
+    input wire 			    di_read_rdy,
+    input [31:0] 		    di_reg_datao,
+
+    output reg 			    di_write,
+    input wire 			    di_write_rdy,
+    output reg 			    di_write_mode,
+    output [31:0] 		    di_reg_datai,
+    input [15:0] 		    di_transfer_status
+
+    );
+
+   parameter STATE_WAIT=0,
+             STATE_SHIFT=1,
+             STATE_ACK=2, 
+             STATE_ACK2=3,
+             STATE_WRITE=4,
+             STATE_CHECK_ACK=5,
+             STATE_SEND=6;
+   
+   reg we, done, busy;
+   reg [REG_DATA_WIDTH-1:0] datao;
+   reg [REG_ADDR_WIDTH-1:0] reg_addr;
+   reg [2:0] state;
+   reg       scl_s, sda_s, scl_ss, sda_ss, sda_reg, oeb_reg;
+   reg [7:0]  sr;
+   reg [1:0]  reg_byte_count;
+   reg [1:0]  addr_byte_count;
+   reg        rw_bit;
+   reg [REG_DATA_WIDTH-1:0] sr_send;
+   reg        nack;
+   reg [6:0]  chip_addr_reg;
+
+   assign scl_oeb = 1;
+   assign scl_out = 0;
+   assign sda_oeb = oeb_reg;
+   assign sda_out = sda_reg;
+
+   assign di_len = REG_DATA_WIDTH;
+
+   wire       open_drain_mode = 1;
+   function set_sda_reg;
+      input   out1;
+      begin
+         set_sda_reg = (open_drain_mode) ? 0 : out1;
+      end
+   endfunction
+   function set_oeb_reg;
+      input   oeb;
+      input   out1;
+      begin
+         set_oeb_reg = (open_drain_mode) ? out1 : oeb;
+      end
+   endfunction
+      
+   always @(posedge clk or negedge reset_n) begin
+      if(!reset_n) begin
+
+      end else begin
+	 if(we) begin
+	    di_reg_addr <= reg_addr;
+	    
+	 end else begin
+	 end
+	 
+      end
+      
+   end
+
+   
+   always @(posedge clk) begin
+      scl_s <= scl_in;
+      scl_ss <= scl_s;
+      sda_s <= sda_in;
+      sda_ss <= sda_s;
+      chip_addr_reg <= chip_addr;
+   end
+
+   wire [7:0] word = { sr[6:0], sda_s };
+   /* verilator lint_off WIDTH */
+   wire [REG_DATA_WIDTH-1:0] word_expanded = word;
+   /* verilator lint_on WIDTH */
+   
+
+   wire       scl_rising  =  scl_s && !scl_ss;
+   wire       scl_falling = !scl_s &&  scl_ss;
+   wire       sda_rising  =  sda_s && !sda_ss;
+   wire       sda_falling = !sda_s &&  sda_ss;
+   
+
+   wire [REG_ADDR_WIDTH+8-1:0] shifted_reg_addr = { reg_addr, word };
+   
+   
+`ifdef SYNC_RESET
+   always @(posedge clk) begin
+`else
+   always @(posedge clk or negedge reset_n) begin
+`endif      
+      if(!reset_n) begin
+	 di_write_mode <= 0;
+	 di_term_addr <= 0;
+         sda_reg <= 1;
+         oeb_reg <= 1;
+         reg_byte_count <= 0;
+	 addr_byte_count <= 0;
+         sr <= 8'h01;
+         state <= STATE_WAIT;
+         datao <= 0;
+         reg_addr <= 0;
+         we   <= 0;
+         rw_bit <= 0;
+         sr_send <= 0;
+         nack <= 0;
+         done <= 0;
+         busy <= 0;
+      end else begin
+         if(scl_ss && sda_falling) begin // start code
+            reg_byte_count <= 0;
+            addr_byte_count <= 0;
+            sr <= 8'h01;
+            state <= STATE_SHIFT;
+            sda_reg <= set_sda_reg(1);
+            oeb_reg <= set_oeb_reg(1, 1);
+            we <= 0;
+            busy <= 1;
+            done <= 0;
+         end else if(scl_ss && sda_rising) begin // stop code
+            state <= STATE_WAIT;
+            sda_reg <= set_sda_reg(1);
+            oeb_reg <= set_oeb_reg(1, 1);
+            we <= 0;
+            if(busy) done <= 1;
+         end else begin
+            if(state == STATE_WAIT) begin
+               done <= 0;
+               we <= 0;
+               reg_byte_count <= 0;
+               addr_byte_count <= 0;
+               sr <= 8'h01; // preload sr with LSB 1.  When that 1 reaches the MSB of the shift register, we know we are done.
+               sda_reg <= set_sda_reg(1);
+               oeb_reg <= set_oeb_reg(1, 1);
+               busy <= 0;
+            end else if(state == STATE_SHIFT) begin
+               sda_reg <= set_sda_reg(1);
+               oeb_reg <= set_oeb_reg(1, 1);
+               if(scl_rising) begin
+                  sr <= word;
+                  if(sr[7]) begin
+		     if(addr_byte_count <= NUM_ADDR_BYTES) begin
+			addr_byte_count <= addr_byte_count + 1;
+
+			if(addr_byte_count == 0) begin // 1st byte (i2c addr)
+			   di_term_addr <= chip_addr_reg;
+                           rw_bit <= word[0];
+                           sr_send <= datai; 
+                           state <= STATE_ACK;
+			end else begin // remaining addr bytes (reg addr)
+                           state <= STATE_ACK;
+                           reg_addr <= shifted_reg_addr[REG_ADDR_WIDTH-1:0];
+			end
+                     end else begin 
+			// LSB of transfer count is used to track which
+			// byte of the 16 bit word is being collected.
+			// MSB of transfer count is only 0 at the begining
+			// of the packet to signal the address is being
+			// collected.  After the address has been received,
+			// then it is all data after that.
+			datao <= (datao << 8) | word_expanded;
+			
+                        if(reg_byte_count == NUM_DATA_BYTES-1) begin // Least significant byte
+                           state <= STATE_WRITE;
+                           we <= 1;
+			   di_write_mode <= 1;
+			   reg_byte_count <= reg_byte_count + 1 - NUM_DATA_BYTES;
+                        end else begin              // Most significant byte
+                           state <= STATE_ACK;
+			   reg_byte_count <= reg_byte_count + 1;
+                        end                     
+                     end
+                  end
+               end
+            end else if(state == STATE_WRITE) begin
+               // Stay here one clock cycle before moving to ACK to
+               // give 'we' a single clock cycle high.
+               state <= STATE_ACK;
+               reg_addr  <= reg_addr + 1; // advance addr for the case of seq writes
+               we    <= 0;
+               sda_reg <= set_sda_reg(1);
+               oeb_reg <= set_oeb_reg(1, 1);
+            end else if(state == STATE_ACK) begin
+               we <= 0;
+               // when scl falls, drive sda low to ack the received byte
+               if(!scl_ss) begin
+                  sda_reg <= set_sda_reg(0);
+                  oeb_reg <= set_oeb_reg(0, 0);
+                  state <= STATE_ACK2;
+                  if(rw_bit && (reg_byte_count == 0)) begin
+		     sr_send <= datai;
+		  end
+               end             
+            end else if(state == STATE_ACK2) begin
+               sr <= 8'h01;
+               we <= 0;
+               // on the falling edge go back to shifting in data
+               if(scl_falling) begin
+                  if(rw_bit) begin // when master is reading, go to STATE_SEND
+                     state <= STATE_SEND;
+                     sda_reg <= set_sda_reg(sr_send[REG_DATA_WIDTH-1]);
+                     oeb_reg <= set_oeb_reg(0, sr_send[REG_DATA_WIDTH-1]);
+                     sr_send <= sr_send << 1;
+                  end else begin // when master writing, receive in STATE_SHIFT
+                     state <= STATE_SHIFT;
+                     sda_reg <= set_sda_reg(1);
+                     oeb_reg <= set_oeb_reg(1, 1);
+                  end
+               end
+            end else if(state == STATE_CHECK_ACK) begin
+               sr <= 8'h01;
+               if(scl_rising) begin
+                  nack <= sda_s;
+               end 
+               if(scl_falling) begin
+                  if(nack) begin
+                     state <= STATE_WAIT; // we received a nack, so we are done
+                     done <= 1;
+                     sda_reg <= set_sda_reg(1);
+                     oeb_reg <= set_oeb_reg(1, 1);
+                  end else begin
+                     state <= STATE_SEND; // we received an ack, so more data requested
+                     sda_reg <= set_sda_reg(sr_send[REG_DATA_WIDTH-1]);
+                     oeb_reg <= set_oeb_reg(0, sr_send[REG_DATA_WIDTH-1]);
+                     sr_send <= sr_send << 1;
+                  end
+               end
+            end else if(state == STATE_SEND) begin
+               if(scl_falling) begin
+                  sr <= word;
+                  if(sr[7]) begin
+                     reg_byte_count <= reg_byte_count + 1;
+                     sda_reg <= set_sda_reg(1);
+                     oeb_reg <= set_oeb_reg(1, 1);
+                     state <= STATE_CHECK_ACK;
+
+                     if(reg_byte_count == NUM_DATA_BYTES-1) begin
+                        reg_addr <= reg_addr + 1; // advance the internal address so that the next address data is available after this transfer.
+			reg_byte_count <= 0;
+                     end
+                     
+
+                  end else begin
+                     sda_reg <= set_sda_reg(sr_send[REG_DATA_WIDTH-1]);
+                     oeb_reg <= set_oeb_reg(0, sr_send[REG_DATA_WIDTH-1]);
+                     sr_send <= sr_send << 1;
+                  end
+               end
+            end
+         end
+      end
+   end 
+endmodule // i2c_slave
